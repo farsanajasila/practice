9e7bd849585829af41b13a7fdb9478c909715cc2
farsanajasila <farsanajasila@gmail.com>
2023-09-17 04:31:01 +0530

    commit

--- lib/13-09-2023.dart
+++ lib/13-09-2023.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MaterialApp(
     home:SplashPage(),
@@ -19,21 +20,26 @@ class SplashPage extends StatelessWidget {
             //   size: 80,
             //   color: Colors.red,
             // ),
-            //  Image(
-            // image: AssetImage("assets/icons/tree.png"),
-            //  width: 100 ,
-            //  height: 100,
-            // ),
+           //    Image(
+           //   image: AssetImage("assets/icons/tree.png"),
+           //   width: 100 ,
+           //    height: 100,
+           //   ),
             Image(
-              image: NetworkImage("https://icons.iconarchive.com/icons/elegantthemes/softies/128/Compose-icon.png"),
-              width: 100,height: 100,
+              image: NetworkImage("https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/template_x_application_office-512.png"),
+             width: 100,height: 100,
             ),
             Text(
               "My Application",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold),
+              style: GoogleFonts.dancingScript(
+                fontSize: 30,
+                color: Colors.lightBlueAccent,
+                fontWeight: FontWeight.bold
+              ),
+              // style: TextStyle(
+              //     fontSize: 20,
+              //     color: Colors.green,
+              //     fontWeight: FontWeight.bold),
             )
           ],
         ),
